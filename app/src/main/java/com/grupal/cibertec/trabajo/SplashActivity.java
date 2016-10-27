a001b84e19e324622bbb292cb57116cf6e3593aa
Grupo Android <grupoandroid@outlook.com>
2016-10-26 21:55:47 -0500

    avance

--- app/src/main/java/com/grupal/cibertec/trabajo/SplashActivity.java
+++ app/src/main/java/com/grupal/cibertec/trabajo/SplashActivity.java
@@ -1,7 +1,8 @@
 package com.grupal.cibertec.trabajo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(4000);
+                    Intent startMainScreen = new Intent( getApplicationContext(), MainActivity.class);
+                    startActivity(startMainScreen);
+                    finish();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+
     }
+
 }
